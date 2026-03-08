a387d41b846ade599e2d41a7d5385394677d8685
Subrata Mondal <subratamondal@tutanota.com>
2026-03-08 18:07:55 +0530

    solve the api key and promise return issue

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,16 @@ const sub = (a: number, b: number): number => {
   return a - b;
 };
 
-// test CodeRabbit
-const getData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/", {
-    headers: {
-      "x-api-key": "f651e791e9d8e92e",
-    },
-  });
-};
-
 // test codeql dot in github
 function echo(userInput: string) {
   execFile("echo", [userInput]);
 }
 
-const getData2 = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts/", {
+const getData = () => {
+  return fetch("https://jsonplaceholder.typicode.com/posts/", {
     headers: {
-      "x-api-key": "f651e791e9d8e92eqwewq",
+      "x-api-key": process.env.API_KEY || "",
     },
   });
 };
-
-export { sum, sub, getData, getData2, echo };
+export { sum, sub, getData, echo };
